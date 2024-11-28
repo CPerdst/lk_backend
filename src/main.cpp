950a073b4775783f9c6452ebb65efcbf0480395b
xefgiyvucn <212086130@qq.com>
2024-11-28 14:31:29 +0800

    修复：解决交叉平台编译下线程库连接错误 并实现基本服务器wq

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,51 @@
 //
 
 #include "eventCapturer.h"
+#include "Server.h"
 
-int main(){
-    RootInfo() << "Hello World!";
+/**
+ * ./server host port[:8080]
+ */
+
+int main(int argc, char* argv[]){
+    std::string host{};
+    unsigned short port = 0;
+    switch (argc) {
+        case 1:
+        {
+            host = "0.0.0.0";
+            port = 8080;
+            break;
+        }
+        case 2:
+        {
+            host = argv[1];
+            port = 8080;
+            break;
+        }
+        case 3:
+        {
+            host = argv[1];
+            unsigned short tmp = std::atoi(argv[2]);
+            if(tmp){
+                port = tmp;
+                break;
+            }
+        }
+        default:
+        {
+            // --help
+            RootInfo() << "usage: \n" << argv[0] << " [host] [port]\ndefault use 0.0.0.0:8080";
+            exit(1);
+        }
+    }
+
+    try{
+        Server server(host, port);
+        server.start();
+    }catch(std::exception& e){
+        RootError() << "Server Crashed as for: " << e.what();
+        exit(1);
+    }
     return 0;
 }
